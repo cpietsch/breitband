cc2ea9cb8b6ebd1b2d663c257644370cfdcd872f
sebastian-meier <contact@sebastianmeier.eu>
2016-01-04 20:37:20 +0100

    Adding the map and the company vis

--- bootstrap/js/map.js
+++ bootstrap/js/map.js
@@ -2,8 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianVsaTg0IiwiYSI6Ik1Lc3pjRlUifQ.qIV8aKI8gqySeJ
 var map = new mapboxgl.Map({
 	container: 'map',
 	center: [13.4244, 52.5047],
-	zoom: 11,
-	minZoom:10,
+	zoom: 7,
 	maxZoom:17,
 	maxBounds:[[13.0883536782043,52.3382388102358],[13.761131111581,52.6755085785852]],
 	style: 'js/map_config.json'
