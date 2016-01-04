f3a725e077b3a8a31b4afa04fe8c6713744b7988
sebastian-meier <contact@sebastianmeier.eu>
2016-01-04 17:15:56 +0100

    Boilerplate start

---
+++ bootstrap/js/map.js
@@ -0,0 +1,15 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianVsaTg0IiwiYSI6Ik1Lc3pjRlUifQ.qIV8aKI8gqySeJOV0GwYiw';
+var map = new mapboxgl.Map({
+	container: 'map',
+	center: [13.4244, 52.5047],
+	zoom: 11,
+	minZoom:10,
+	maxZoom:17,
+	maxBounds:[[13.0883536782043,52.3382388102358],[13.761131111581,52.6755085785852]],
+	style: 'js/map_config.json'
+});
+
+//Add Geocoder Search Box
+map.addControl(new mapboxgl.Geocoder({keepOpen: true, label:'Suche'}));
+//Change the placeholder to german
+d3.select('.mapboxgl-ctrl-geocoder input').attr('placeholder', 'Suche');
