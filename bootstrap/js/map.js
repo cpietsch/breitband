10841a8011bcf04be7ff9dc8fbc2994627e28916
sebastian-meier <contact@sebastianmeier.eu>
2016-01-12 18:27:12 +0100

    dirty map blocker

--- bootstrap/js/map.js
+++ bootstrap/js/map.js
@@ -2,10 +2,10 @@ mapboxgl.accessToken = 'pk.eyJ1IjoianVsaTg0IiwiYSI6Ik1Lc3pjRlUifQ.qIV8aKI8gqySeJ
 var map = new mapboxgl.Map({
 	container: 'map',
 	center: [13.4244, 52.5047],
-	zoom: 7,
+	zoom: 10,
 	maxZoom:17,
-	maxBounds:[[13.0883536782043,52.3382388102358],[13.761131111581,52.6755085785852]],
-	style: 'js/map_config.json'
+	minZoom:10,
+	style: 'js/map_config_mapbox_only.json'
 }).on('click', 
 	function(e){
 		mapEnable();
@@ -28,6 +28,6 @@ function mapEnable(){
 var mapStatus = false;
 
 //Add Geocoder Search Box
-map.addControl(new mapboxgl.Geocoder({keepOpen: true, label:'Suche'}));
+//map.addControl(new mapboxgl.Geocoder({keepOpen: true, label:'Suche'}));
 //Change the placeholder to german
-d3.select('.mapboxgl-ctrl-geocoder input').attr('placeholder', 'Suche');
+//d3.select('.mapboxgl-ctrl-geocoder input').attr('placeholder', 'Suche');
