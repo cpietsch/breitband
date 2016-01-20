d55bc9610d836e8e58903fb77869a777ae89a511
sebastian-meier <contact@sebastianmeier.eu>
2016-01-20 15:27:12 +0100

    pulse testing

--- final/js/main.js
+++ final/js/main.js
@@ -33,3 +33,14 @@ function debouncer( func , timeout ) {
 		} , timeout );
 	};
 }
+
+window.requestAnimationFrame = (function() {
+  return window.requestAnimationFrame ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame ||
+         window.oRequestAnimationFrame ||
+         window.msRequestAnimationFrame ||
+         function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+           return window.setTimeout(callback, 1000/60);
+         };
+})();
